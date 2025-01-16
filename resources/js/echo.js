c173515543370c632dbf8cf4dc75b7eec7d8e543
jomar godinez <jomargodinez70@gmail.com>
2025-01-16 14:57:12 +0800

    fix: modify the broadcasting echo in the forntend side

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,5 +1,4 @@
 import Echo from 'laravel-echo';
-
 import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
@@ -7,8 +6,10 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsPort: import.meta.env.VITE_REVERB_PORT || 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT || 443,
+    forceTLS: true,  // Set this to true for production
     enabledTransports: ['ws', 'wss'],
+    cluster: null,   // Important for Reverb
+    disableStats: true,
 });
